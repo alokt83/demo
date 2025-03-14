e88e978cc6b29514d2f9c7a305ec591454197d4d
alokt83 <alok.tanna@inniti.in>
2025-03-13 20:15:51 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket83"
+    key           = "terraform.tfstate"
+    region        = "us-east-1"
+    dynamodb_table = "terraform-lock"
+    encrypt       = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-secure-terraform-bucket"
+  acl    = "private"
+}
