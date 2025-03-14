7b0bbaba497eaaad6c544d347717477441906df1
alokt83 <alok.tanna@inniti.in>
2025-03-13 20:24:45 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket83"
+    bucket         = "my-terraform-state-bucket0313"
     key           = "terraform.tfstate"
     region        = "us-east-1"
     dynamodb_table = "terraform-lock"
